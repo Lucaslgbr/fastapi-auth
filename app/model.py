e7403220dac5f62c5167024445f8f58bc1a03c28
lucas <lucas@singular.inf.br>
2024-05-18 13:27:30 -0300

    feat: api de login e signup com jwt

---
+++ app/model.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, Field, EmailStr
+
+
+class UserSchema(BaseModel):
+    fullname: str = Field(...)
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "fullname": "André Cristen",
+                "email": "admin@admin.com",
+                "password": "teste"
+            }
+        }
+
+class UserLoginSchema(BaseModel):
+    email: EmailStr = Field(...)
+    password: str = Field(...)
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "email": "admin@admin.com",
+                "password": "teste"
+            }
+        }

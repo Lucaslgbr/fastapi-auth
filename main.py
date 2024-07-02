30a0001a5bc402588f07d30dd6b8921bab56f399
lucas <lucas@singular.inf.br>
2024-07-02 20:35:05 -0300

    feat: dados para grafana e correção banco

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import uvicorn
 from app.api import app
+from prometheus_fastapi_instrumentator import Instrumentator
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+
+    
+Instrumentator().instrument(app).expose(app)
